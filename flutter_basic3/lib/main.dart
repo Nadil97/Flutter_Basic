dfda58de1c29c4cc5b6251dc411de39de54efa4e
nadilsandaruwan97@gmail.com <87482209+Nadil97@users.noreply.github.com>
2022-10-21 11:28:09 +0530

    add screens

--- flutter_basic3/lib/main.dart
+++ flutter_basic3/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
+import 'screens/homepage.dart';
+
 void main() {
   runApp(App());
 }
@@ -14,36 +16,6 @@ class App extends StatelessWidget {
       title: "project 03",
       home: Homepage(
         title: 'Flutter navigation',
-      ),
-    );
-  }
-}
-
-class Homepage extends StatefulWidget {
-  const Homepage({required this.title}); //: super(key: key);
-  final String title;
-  @override
-  State<Homepage> createState() => _HomepageState();
-}
-
-class _HomepageState extends State<Homepage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Text(
-            "You have pushed button time",
-            style: TextStyle(fontSize: 25.0),
-          ),
-        ]),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {},
       ),
     );
   }
---
+++ flutter_basic3/lib/screens/homepage.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({required this.title}); //: super(key: key);
+  final String title;
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Text(
+            "You have pushed button time",
+            style: TextStyle(fontSize: 25.0),
+          ),
+        ]),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+    );
+  }
+}
